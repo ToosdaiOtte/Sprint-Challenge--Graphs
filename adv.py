1d58a4df9f8d9e6731379c5c765c9e26e63d5177
Toosdai Otte <toosdai52Otte@gmail.com>
2020-11-22 22:27:52 -0600

    start-added stack class

--- adv.py
+++ adv.py
@@ -29,7 +29,21 @@ player = Player(world.starting_room)
 # traversal_path = ['n', 'n']
 traversal_path = []
 
+class Stack():
+    def __init__(self):
+        self.stack = []
+    
+    def push(self, value):
+        self.stack.append(value)
 
+    def pop(self):
+        if self.size() > 0:
+            return self.stack.pop()
+        else:
+            return None
+
+    def size(self):
+        return len(self.stack)
 
 # TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
